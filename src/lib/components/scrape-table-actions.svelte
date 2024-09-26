9fca476ea51fdc50ea15e70c86a9a1bd34278813
Filip Trplan <filip@trplan.si>
2024-09-26 21:36:19 +0200

    fix: remove magnet button from table

--- src/lib/components/scrape-table-actions.svelte
+++ src/lib/components/scrape-table-actions.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { Magnet, Plus } from 'lucide-svelte';
-	import { toast } from 'svelte-sonner';
+	import { Plus } from 'lucide-svelte';
 
 	export let onAddMagnet: (magnet: string) => void;
 	export let magnet: string;
@@ -17,16 +16,5 @@
 		}}
 	>
 		<Plus class="size-4" />
-	</Button>
-	<Button
-		variant="ghost"
-		class="px-2"
-		size="sm"
-		on:click={() => {
-			navigator.clipboard.writeText(magnet);
-			toast.success('Magnet copied to clipboard');
-		}}
-	>
-		<Magnet class="size-4" />
 	</Button>
 </div>
